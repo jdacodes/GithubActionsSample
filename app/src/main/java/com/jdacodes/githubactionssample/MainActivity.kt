f330ece12448f7728394763da1d481ac17227470
jdacodes <jda.codes@gmail.com>
2025-02-13 14:53:38 +0800

    No error, executed check gradle task

--- app/src/main/java/com/jdacodes/githubactionssample/MainActivity.kt
+++ app/src/main/java/com/jdacodes/githubactionssample/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GithubActionsSampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize())) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding),
