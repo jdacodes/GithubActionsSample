b1ba003968f83cf8a3f5b122a681653ca03d551a
jdacodes <105356546+jdacodes@users.noreply.github.com>
2025-02-13 14:41:57 +0800

    Revert "Break CI by wrong format"

--- app/src/main/java/com/jdacodes/githubactionssample/MainActivity.kt
+++ app/src/main/java/com/jdacodes/githubactionssample/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)),
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
